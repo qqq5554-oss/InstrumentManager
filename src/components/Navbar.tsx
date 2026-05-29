f976c6ba8406a3f85aab44cb89d51210aa135726
qqq5554-oss <qoo503@gmail.com>
2026-05-29 17:44:08 +0800

    Fix logo import path and add missing vite-env.d.ts

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { NavLink } from 'react-router-dom'
-import logo from '../assets/logo.png'
+import logo from '../logo.png'
 
 export default function Navbar() {
   const linkClass = ({ isActive }: { isActive: boolean }) =>
